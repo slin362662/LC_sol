3cbb01ac03eaa0830bf81875c94a7e20bd7befab
Simo Lin <simo.lin@oracle.com>
2020-03-08 19:42:40 -0700

    sol for 2

---
+++ 2. Add Two Numbers/add_two_num.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        cur = head
+        sum = 0
+
+        while l1 or l2:
+            sum /= 10
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            cur.next = ListNode(sum%10)
+            cur = cur.next
+
+        if sum / 10 == 1:
+            cur.next = ListNode(1)
+            cur = cur.next
+        return head.next
